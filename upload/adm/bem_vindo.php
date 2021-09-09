270846400a96d8b357cbeb51bd272f9287de8e06
Claudio Ribeiro <claudio115789@gmail.com>
2021-09-09 01:40:21 -0300

    V1
    versao 1

---
+++ upload/adm/bem_vindo.php
@@ -0,0 +1,12 @@
+ 
+ 
+ 
+
+ 
+<DIV class="">
+<img src="img/bem-vindo2.jpg" alt="" class="img-responsive center-block"/>
+</DIV>
+		
+	
+
+
---
+++ upload/adm/menu_admin.php
@@ -0,0 +1,42 @@
+<!-- Inicio navbar -->
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a class="navbar-brand" href="administrativo.php">Grasa</a>
+	</div>
+	<div id="navbar" class="navbar-collapse collapse">
+	  <ul class="nav navbar-nav">            
+		<li class="dropdown">
+		  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Usuário <span class="caret"></span></a>
+		  <ul class="dropdown-menu">
+			<li><a href="administrativo.php?link=2">Listar</a></li>			
+			<li><a href="administrativo.php?link=18">Nivel de Acesso</a></li>   
+			
+		  </ul>
+		</li>
+		
+		<li class="dropdown">
+		  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Documento <span class="caret"></span></a>
+		  <ul class="dropdown-menu">
+			<li><a href="administrativo.php?link=10">Listar Documento</a></li> 
+			<li><a href="administrativo.php?link=7">Listar Categoria</a></li> 
+		  </ul>
+		</li>
+		<li class="dropdown">
+		  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Configuração <span class="caret"></span></a>
+		  <ul class="dropdown-menu">
+			<li><a href="administrativo.php?link=14">Situação</a></li>
+		  </ul>
+		</li>
+		<li><a href="../../index.html">Sair</a></li>
+	  </ul>
+	</div><!--/.nav-collapse -->
+  </div>
+</nav>
+<!-- Fim navbar -->
